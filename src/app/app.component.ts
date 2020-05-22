c233fceee02562d960c3958546fbcc68580bb935
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2020-05-21 18:29:26 -0600

    assignment-6-template-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: []
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["./app.style.scss"],
 })
 export class AppComponent {
-  title = 'aa-angular-study';
+  user: any = {
+    email: "",
+    plan: "basic",
+    password: "",
+  };
+
+  onSubmitForm(f: NgForm) {}
 }
